a60cab93c7d01dd7f0467d8ba1260d9db4549638
kowshik007 <kowshikkondapaneni@gmail.com>
2020-04-26 15:30:43 +0530

    04/26/2020 [Kowshik] SocietyManagement angular project with bulma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { RegisterComponent } from './register/register.component';
+import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/index/index.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styles: [`
+        .green{
+            color: white;
+        }
+    `]
+})
+export class IndexComponent{
+    message: string="Need to manage a big society! No problem I am here"
+    name: string="Kowshik"
+    showButton: boolean=false;
+    constructor(){
+        setTimeout(() => {
+            this.showButton=true;
+        }, 2000);
+    }
+    onButtonClick(){
+        this.message="Welcome "+this.name;
+        return "You have clicked a button";
+    }
+    onChangingName(event: Event){
+        this.name=(<HTMLInputElement>event.target).value;
+    }
+    getWelcomeMessage(){
+        return this.message;
+    }
+    displayColor(){
+        return 'green';
+    }
+}
